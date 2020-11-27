eaa88aa5cbf37163910f0270cf88581312a68ba2
kyungmin <kimkyongmin2@daum.net>
2020-11-27 22:03:39 +0900

    로그인 기능 수정

--- src/test/java/com/egg/tfox/login/test.java
+++ src/test/java/com/egg/tfox/login/test.java
@@ -18,37 +18,37 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/security-context.xml" })
 public class test {
 	@Autowired
 	PasswordEncoder encoder;
-	
+
 	@Autowired
 	DataSource ds;
-	
-	@Test
-	public void test()
-	{
+
+	// @Test
+	public void test() {
 		log.info("{}", encoder);
 		log.info("{}", ds);
 	}
-	
-//	@Test
+
+	@Test
 	public void test2() {
 		Connection con = null;
 		PreparedStatement pstmt = null;
-		
+
 		String query = "update employee set emp_password=? where emp_loginid=?";
-		
-		for (int j = 1; j <= 10; j++) {
+
+		for (int j = 1; j <= 6; j++) {
 			try {
 				con = ds.getConnection();
 				pstmt = con.prepareStatement(query);
-				
-//				pstmt.setString(1, "test" + j);
-				pstmt.setString(1, encoder.encode("test" + j));
-				pstmt.setString(2, "test" + j);
-				
+
+				// pstmt.setString(1, "epass" + 01);
+				pstmt.setString(1, encoder.encode("epass" + 0 + j));
+				pstmt.setString(2, "euser" + 0 + j);
+
 				pstmt.executeUpdate();
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
@@ -62,5 +62,51 @@ public class test {
 				}
 			}
 		}
+
+			for (int i = 1; i <= 6; i++) {
+				try {
+					con = ds.getConnection();
+					pstmt = con.prepareStatement(query);
+
+					// pstmt.setString(1, "epass" + 01);
+					pstmt.setString(1, encoder.encode("bpass" + 0 + i));
+					pstmt.setString(2, "buser" + 0 + i);
+
+					pstmt.executeUpdate();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally {
+					try {
+						pstmt.close();
+						con.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			for (int k = 1; k <= 6; k++) {
+				try {
+					con = ds.getConnection();
+					pstmt = con.prepareStatement(query);
+
+					// pstmt.setString(1, "epass" + 01);
+					pstmt.setString(1, encoder.encode("mpass" + 0 + k));
+					pstmt.setString(2, "muser" + 0 + k);
+
+					pstmt.executeUpdate();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} finally {
+					try {
+						pstmt.close();
+						con.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		
 	}
 }
