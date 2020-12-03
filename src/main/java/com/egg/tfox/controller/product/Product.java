c1e6afa725b14089647de71d67d7c9f366968eb8
kyungmin <kimkyongmin2@daum.net>
2020-12-03 17:51:07 +0900

    모델

--- src/main/java/com/egg/tfox/controller/product/Product.java
+++ src/main/java/com/egg/tfox/controller/product/Product.java
@@ -14,6 +14,7 @@ public class Product {
 	}
 
 	
+	
 		@PostMapping("/product/insert")
 	  @ResponseBody 
 	  String addManu(String name, String CEO) {
--- src/main/webapp/WEB-INF/views/product/modal_Manufacturer.jsp
+++ src/main/webapp/WEB-INF/views/product/modal_Manufacturer.jsp
@@ -36,6 +36,7 @@
           	 
          
         }
+        
      
         .close:hover,
         .close:focus {
